74bc99bc8ec13617cc5f150ea9d20f072eda774f
zmchang <zheming2002@gmail.com>
2024-07-10 18:06:49 +0800

    added function to store data in db

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,52 @@
 from flask import Flask, jsonify, request
+# from flask_cors import CORS
+import sqlite3
+
 
 app = Flask(__name__) 
+# CORS(app)
+
+DATABASE = 'backend/database.db'
 
 #Function stubs:
 def perform_ai_processing(data):
     # Perform AI processing on the input data
+
+    # Returns json with predicted classes and respective confidence scores
     return {'result': 'AI processing result'}
 
+# Store the input and result in the database
 def store_in_database(data, result):
-    # Store the input and result in the database
-    pass
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+
+    # Store image data in Images table
+    cursor.execute(f'''
+        INSERT INTO Images (filepath) VALUES {data}
+    ''')
+    connection.commit()
+    image_id = cursor.lastrowid
+
+    # Store inference data in Results table
+    rows = []
+    for pred in result['result']:
+        rows.append((image_id, pred['label_id'], pred['confidence_score']))
+    cursor.executemany(f'''
+        INSERT INTO Results (image_id, label_id, confidence_score) VALUES (?, ?, ?)
+    ''', rows)
+    result_id = cursor.lastrowid
+
+    return image_id, result_id
 
 def fetch_all_results():
     # Fetch all results from the database
     return [{'input': 'input1', 'result': 'result1'}, {'input': 'input2', 'result': 'result2'}]
 
 
-@app.route('/analyze', methods=['POST'])
+@app.route('/infer', methods=['POST'])
 # POST /analyze: Accepts user input, performs AI processing, stores the input and result in the database, and returns the result.
-def analyze():
-    # Access the raw data from the request
+def infer():
+    # Access the raw data (file path) from the request
     data = request.data
     # Check if data is present
     if not data:
@@ -30,7 +57,9 @@ def analyze():
     result = perform_ai_processing(data)
 
     # Store the input and result in the database
-    store_in_database(data, result)
+    image_id, result_id = store_in_database(data, result)
+    if not (image_id and result_id):
+        return jsonify({'error': 'failed to add data to database'}), 400
 
     # Return the result
     return jsonify(result), 200
